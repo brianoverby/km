b13f0d3cd78e76e43962f88e3a579af1fabba842
Brian Overby <mail@brianoverby.dk>
2023-02-16 12:09:18 +0100

    Added 36 key layout

--- users/bov_split36/bov_split36.c
+++ users/bov_split36/bov_split36.c
@@ -7,9 +7,9 @@
 // Define keymap layers
 enum layers {
     _BASE,
+    _SYM,
     _NAV,
-    _NUM,
-    _SYM
+    _FUN
 };
 
 // Define custom keycodes
@@ -60,23 +60,30 @@ enum keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_BASE] = LAYOUT_bov_split42(
-        KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
-        KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    DK_SCLN, DK_QUOT,
-        KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    COMMA,   DOT,     DK_SLSH, KC_TAB,
-                                            KC_LCMD, LA_NAV,  KC_SPC,           KC_ENT,  LA_SYM,  KC_LALT   
+        XXXXXXX, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    XXXXXXX,
+        XXXXXXX, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_BSPC, XXXXXXX,
+        XXXXXXX, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    COMMA,   DOT,     KC_ENT,  XXXXXXX,
+                                            KC_LCMD, LA_SYM,  KC_LSFT,          KC_SPC,  LA_NAV,  KC_LALT   
     ),
-
-    [_NAV] = LAYOUT_bov_split42(
-        KC_TAB,  LOCK,    SW_WIN,  SEARCH,  KC_VOLD, KC_VOLU,                            KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, DK_AA,
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                              KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, DK_AE,   DK_OE,
-        _______, KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,                             KC_F11,  KC_F12,  XXXXXXX, XXXXXXX, XXXXXXX, _______,
-                                            _______, _______, _______,          _______, KC_DEL,  _______
-    ),
-
+    
     [_SYM] = LAYOUT_bov_split42(
-        _______, DK_EXLM, AT,      DK_HASH, DLR,     DK_PERC,                            DK_CIRC, DK_AMPR, DK_ASTR, DK_LPRN, DK_RPRN, BSLS,
-        _______, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               DK_MINS, DK_EQL,  DK_GRV,  LCBR,    RCBR,    PIPE,
-        _______, KC_6,    KC_7,    KC_8,    KC_9,    KC_0,                               DK_UNDS, DK_PLUS, DK_TILD, DK_LBRC, DK_RBRC, _______,
+        XXXXXXX, DK_EXLM, AT,      DK_HASH, DLR,     DK_PERC,                            DK_LPRN, DK_RPRN, DK_CIRC, DK_QUES, DK_TILD, XXXXXXX,
+        XXXXXXX, DK_AMPR, DK_ASTR, DK_UNDS, DK_QUOT, DK_DQUO,                            LCBR,    RCBR,    DK_DIAE, DK_ACUT, DK_GRV,  XXXXXXX,
+        XXXXXXX, PIPE,    DK_PLUS, DK_MINS, DK_EQL,  BSLS,                               DK_LBRC, DK_RBRC, DK_SCLN, DK_COLN, DK_SLSH, XXXXXXX,
+                                            _______, _______, _______,          _______, _______, _______
+    ),
+    
+    [_NAV] = LAYOUT_bov_split42(
+        XXXXXXX, KC_TAB,  KC_7,    KC_8,    KC_9,    KC_COMM,                            KC_PGUP, KC_HOME, KC_UP,   KC_END,  XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_ESC,  KC_4,    KC_5,    KC_6,    KC_DOT,                             KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, KC_DEL,  XXXXXXX,
+        XXXXXXX, XXXXXXX, KC_1,    KC_2,    KC_3,    KC_0,                               XXXXXXX, DK_AE,   DK_OE,   DK_AA,   KC_ENT,  _______,
+                                            _______, _______, _______,          _______, _______, _______
+    ),
+
+    [_FUN] = LAYOUT_bov_split42(
+        XXXXXXX, KC_F1,   KC_F2,   KC_F3,   KC_F4,   XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_F5,   KC_F6,   KC_F7,   KC_F8,   XXXXXXX,                            KC_LCTL, KC_LALT, KC_LSFT, XXXXXXX, XXXXXXX, XXXXXXX,
+        XXXXXXX, KC_F9,   KC_F10,  KC_F11,  KC_F12,  XXXXXXX,                            XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX, XXXXXXX,
                                             _______, _______, _______,          _______, _______, _______
     )
 };
---
+++ users/bov_split36/rules.mk
@@ -0,0 +1,26 @@
+# Fix after QMK Breaking Changes 2022 August 27: https://github.com/qmk/qmk_firmware/pull/17559/files
+INTROSPECTION_KEYMAP_C = bov_split36.c
+
+SRC += features/swapper.c
+SRC += features/layer_lock.c
+
+VPATH += keyboards/gboards
+
+EXTRAKEY_ENABLE = yes
+KEY_OVERRIDE_ENABLE = yes
+COMBO_ENABLE = yes
+MAGIC_ENABLE = yes
+CAPS_WORD_ENABLE = yes
+
+TAP_DANCE_ENABLE = no
+BOOTMAGIC_ENABLE = no
+MOUSEKEY_ENABLE = no
+RGBLIGHT_ENABLE = no
+LEADER_ENABLE = no
+CONSOLE_ENABLE = no
+COMMAND_ENABLE = no
+AUDIO_ENABLE = no
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no 
+
+LTO_ENABLE = no
